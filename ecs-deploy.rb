c7b510610b7e0fd5f1a365864ad00368f394978a
Phillip Shipley <phillip.shipley@gmail.com>
2018-07-25 09:35:51 -0400

    update ecs-deploy to 3.5.0

--- ecs-deploy.rb
+++ ecs-deploy.rb
@@ -4,9 +4,9 @@
 class EcsDeploy < Formula
   desc "Simple shell script for initiating blue-green deployments on Amazon EC2 Container Service (ECS)"
   homepage "https://github.com/silinternational/ecs-deploy"
-  url "https://github.com/silinternational/ecs-deploy/archive/3.4.0.tar.gz"
-  version "3.4.0"
-  sha256 "9a93fdb54ca5ce44ec691a787ee4ba07c6e5e63920e316190ac8a39034536b3f"
+  url "https://github.com/silinternational/ecs-deploy/archive/3.5.0.tar.gz"
+  version "3.5.0"
+  sha256 "96f8bd529063460d53360a1dcd377647c506c69868bebb5694e69a1321e0a693"
 
   depends_on "awscli"
   depends_on "jq"

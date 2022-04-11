1c088041eb47e74db87dcf2fa5738d0ae2deb069
Matt Henderson <matt_henderson@sil.org>
2022-04-11 10:00:03 -0400

    Update to version 3.10.4

--- ecs-deploy.rb
+++ ecs-deploy.rb
@@ -4,11 +4,11 @@
 class EcsDeploy < Formula
   desc "Simple shell script for initiating blue-green deployments on Amazon EC2 Container Service (ECS)"
   homepage "https://github.com/silinternational/ecs-deploy"
-  url "https://github.com/silinternational/ecs-deploy/archive/3.10.2.tar.gz"
-  version "3.10.2"
+  url "https://github.com/silinternational/ecs-deploy/archive/refs/tags/3.10.4.tar.gz"
+  version "3.10.4"
   # When updating the version, get a SHA-256 hash of the file at the (new) url
   # above and use that as the new sha256 value here:
-  sha256 "b697e024ab653ffad44c87fc39ba96d967d15b40ad7329549a95423a9020f722"
+  sha256 "5b5283cc536142de1756b46f16ef7083cdbe72edf1a33aa7963eba077182cef5"
 
   depends_on "awscli"
   depends_on "jq"
